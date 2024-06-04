15ad9d93c066200d9335b782ebc98bd5b363154c
Mika Korpela <mika.korpela@gofore.com>
2024-06-04 18:51:12 +0300

    commit

---
+++ arrays/print-array.py
@@ -0,0 +1,17 @@
+ducks = [
+    {
+        "uid": 1,
+        "name": "Duey"
+    },
+    {
+        "uid": 2,
+        "name": "Huey"
+    },
+    {
+        "uid": 3,
+        "name": "Luey"
+    }
+]
+
+for duck in ducks:
+    print("{} {}".format(duck["uid"], duck["name"]))
